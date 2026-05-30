db82cb117b6015a0266748db429f5692f5b405c9
Harish S <harishse616@gmail.com>
2026-05-30 06:35:11 -0700

    Serve static files from Vercel public directory

--- blackplane/settings.py
+++ blackplane/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "public" / "static"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CSRF_TRUSTED_ORIGINS = []
